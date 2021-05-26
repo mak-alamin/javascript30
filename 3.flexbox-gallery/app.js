dd32d6b2137c2ebe28297a01780d4c42db73c476
Mak Alamin <makalamin012@gmail.com>
2021-05-26 23:19:09 +0600

    Array Practice

--- 3.flexbox-gallery/app.js
+++ 3.flexbox-gallery/app.js
@@ -1,11 +1,14 @@
 let panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
-    panel.addEventListener("click", function(){
+    panel.addEventListener("click", function(e){
+        console.log(e);
         this.classList.toggle('open');
     });
 
-    panel.addEventListener("transitionend", function(){
-        this.classList.toggle('active');
+    panel.addEventListener("transitionend", function(e){
+        if(e.propertyName.includes('flex')){
+            this.classList.toggle('active');
+        }
     })
 });
