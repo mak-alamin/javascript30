9cc25f58fa63f574909ff92175c5a60dbe666963
Mak Alamin <makalamin012@gmail.com>
2021-07-12 03:26:47 +0600

    Played with HTML canvas

--- 8.Fun-with-HTML5-Canvas/app.js
+++ 8.Fun-with-HTML5-Canvas/app.js
@@ -3,28 +3,47 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = "green";
+// ctx.strokeStyle = `hsl(255, 100%, 50%)`;
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 30;
+ctx.lineWidth = 5;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function lineDraw(e){
     if( ! isDrawing ) {return;}
+    
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
     ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.moveTo(lastX, lastY); //starting point
+    ctx.lineTo(e.offsetX, e.offsetY); //ending point 
+
     ctx.stroke();
 
     [lastX, lastY] = [e.offsetX, e.offsetY];
+    
+    if( hue > 360){
+        hue = 0;
+    }
+
+    hue++;
+
+    if(ctx.lineWidth > 200){
+        ctx.lineWidth = 5;
+    }
+
+    ctx.lineWidth = ctx.lineWidth + 0.5 ;
 }
 
 canvas.addEventListener('mousedown', (e) => {
+
     isDrawing = true;
+    ctx.lineWidth = 5;
+
     [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
